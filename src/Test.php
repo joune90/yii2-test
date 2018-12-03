4e09220b1425051cbfed9b53d6613a529587929d
joune90 <67150798@qq.com>
2018-12-03 15:26:04 +0800

    Initial commit

---
+++ src/Test.php
@@ -0,0 +1,15 @@
+<?php
+namespace joune90\test;
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/12/3
+ * Time: 15:24
+ */
+
+class Test
+{
+    public function test(){
+        echo 'test';
+    }
+}
