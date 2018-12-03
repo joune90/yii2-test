26a808aee5d894f71241f8733b2d1ced14d8e768
joune90 <67150798@qq.com>
2018-12-03 15:49:12 +0800

    test

--- src/Test.php
+++ src/Test.php
@@ -6,10 +6,33 @@ namespace joune90\test;
  * Date: 2018/12/3
  * Time: 15:24
  */
+use yii\base\Component;
 
-class Test
+class Test extends Component
 {
-    public function test(){
-        echo 'test';
+    private $aipNFace = null;
+    public $access_key = '';
+    public $secret_key = '';
+
+    public function init()
+    {
+        if (null === $this->aipNFace) {
+            $this->aipNFace = new AipNFace($this->access_key, $this->secret_key);
+        }
+        return $this->aipNFace;
     }
+
+    //获取分组
+    public function GetGroup($start = 0, $num = 1)
+    {
+        $this->getAipHandler();
+        return $this->aipNFace->GetGroup($start, $num);
+    }
+
+    //获取分组下的所有用户
+    public function GetGroupUsers($groupId)
+    {
+        return $this->aipNFace->GetGroupUsers($groupId);
+    }
+
 }
