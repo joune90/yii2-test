2df0242f9e4a012e3045ba660deb607ea468f1fc
joune90 <67150798@qq.com>
2018-12-03 16:19:37 +0800

    test

---
+++ src/AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace joune90\test;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
